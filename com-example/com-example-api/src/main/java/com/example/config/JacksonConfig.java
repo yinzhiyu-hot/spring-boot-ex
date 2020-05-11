d8d2a8a8e80c6cedb5e26fee00382253cd9f8d88
yinzhiyu <thomasyin@wangoon.com>
2020-05-11 15:30:56 +0800

    API返回值去除为NULL的字段

---
+++ com-example/com-example-api/src/main/java/com/example/config/JacksonConfig.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @Description 返回json空值去掉null和""
+ * @PackagePath com.example.config.JacksonConfig
+ * @Author YINZHIYU
+ * @Date 2020/5/11 15:28
+ * @Version 1.0.0.0
+ **/
+@Configuration
+public class JacksonConfig {
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+}
